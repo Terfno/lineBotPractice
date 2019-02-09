82e379fdc308c1bdeb3f53abeeebf4ec5d8b5d38
terfno <sueda.takahito@gmail.com>
2019-02-09 10:54:04 +0900

    write server js

--- server.js
+++ server.js
@@ -1,5 +1,37 @@
 require('dotenv').config()
+
 const LINE_ACCESS_TOKEN = process.env.LINE_ACCESS_TOKEN
 
-console.log(LINE_ACCESS_TOKEN)
+const express = require('express")
+const bodyParser = require('body-parser')
+const requet = require('request')
+
+const app = express()
+
+app.post('/webhook', (req,res,next) => {
+	res.status(200).end()
+	for(var event of req.body.events){
+		if(event.type == 'message' && event.message.text == 'hello'){
+			var headers = {
+				'Content-Type':'application/json',
+				'Authorization':'Bearer'+LINE_CHANNEL_ACCESS_TOKE
+			}
+			var body = {
+				replyToken:event.replyToken,
+				messages:[{
+					type:'text',
+					text:'world'
+				}]
+			}
+			var url = 'https://api.line.me/v2/bot/message/reply'
+			request({
+				url:url,
+				method:'POST',
+				headers:headers,
+				body:body,
+				json:true
+			})
+		}
+	}
+})
 
