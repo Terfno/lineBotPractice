fca12a2297217d134945add8795567b2673e3526
terfno <sueda.takahito@gmail.com>
2019-02-09 11:58:57 +0900

    fix js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const line - require('@line/bot-sdk')
+require('dotenv').config()
+
+const config = {
+	channelAccessToken:process.env.LINE_ACCESS_TOKEN
+	channelSecret:process.env.LINE_SECRET
+}
+
+const app = express()
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+	Promise
+		.all(req.body.events.map(handleEvent))
+		.then((result) => res.json(result))
+})
+
+const client = new line.Client(confg)
+var handleEvent = (event) => {
+	if(event.type !== 'message' || event.message.type !== 'text'){
+		return Promise.resolve(null)
+	}
+
+	return client.replyMessage(event.replyToken, {
+		type:'text',
+		text: event.message.text
+	})
+}
+
+app.listen(3000)
+
